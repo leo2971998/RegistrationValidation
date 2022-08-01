70f8f17153bcd88b586624fb9882f44d53d31937
leo2971998 <106453722+leo2971998@users.noreply.github.com>
2022-08-01 14:25:48 -0500

    Updated

--- src/Classes/Program.java
+++ src/Classes/Program.java
@@ -1,2 +1,22 @@
-package Classes;public class Program {
+package Classes;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Program {
+    public static boolean ValidateRegistrationNo(String registrationNo) {
+        String regex = "^[A-Z]{2}\\s\\d{1,2}\\s[A-Z]{1,2}\\s\\d{1,4}$";
+        Pattern p = Pattern.compile(regex);
+        if (registrationNo == null){
+            return false;
+        }
+        Matcher m = p.matcher(registrationNo);
+        return m.matches();
+    }
+    public static String getResults(String registrationNo) {
+        if (ValidateRegistrationNo(registrationNo)) {
+            return "Registration No. is valid";
+        }
+        else return "Registration No. is invalid";
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import Classes.*;
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of registration nos: ");
+        int num = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Enter Registration nos: ");
+        List<String> R = new ArrayList<String>();
+        for (int i = 0; i < num; i++) {
+            R.add(sc.nextLine());
+        }
+        R.stream().forEach(T -> System.out.println(String.format("%15s %30s", T, Program.getResults(T))));
     }
 }
